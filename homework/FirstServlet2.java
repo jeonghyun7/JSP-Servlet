87a051f450e91c43b8561708c1f2660b57816f05
jeonghyun Lee <110165263+jeonghyun7@users.noreply.github.com>
2022-08-06 23:27:40 +0900

    Add files via upload

---
+++ homework/FirstServlet2.java
@@ -0,0 +1,34 @@
+package sec01.ex01;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first2")
+public class FirstServlet2 extends HttpServlet {
+	
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+
+	public void destroy() {
+		
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		RequestDispatcher dispatch = 
+				request.getRequestDispatcher("second2?name=lee");
+		dispatch.forward(request, response);
+		
+	}
+
+}
---
+++ homework/FirstServlet3.java
@@ -0,0 +1,34 @@
+package sec01.ex01;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first3")
+public class FirstServlet3 extends HttpServlet {
+	
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+
+	public void destroy() {
+		
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		request.setAttribute("address", "서울시 성북구");
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher("second3");
+		dispatch.forward(request, response);
+	}
+
+}
